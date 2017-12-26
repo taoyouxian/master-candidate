e73430f49d5b28ea38e67ebe1c2c65d98a300433
taoyouxian <taoyouxian@aliyun.com>
2017-12-26 21:14:50 +0800

    fix

--- mc-bbm/src/main/java/cn.edu.ruc.iir.mc.bbm/Client.java
+++ mc-bbm/src/main/java/cn.edu.ruc.iir.mc.bbm/Client.java
@@ -10,29 +10,31 @@ import org.apache.spark.sql.SparkSession;
 /**
  * @version V1.0
  * @Package: cn.edu.ruc.iir.mc.bbm
- * @ClassName: Main
- * @Description: -Dspark.master=local
+ * @ClassName: Client
+ * @Description:
  * @author: taoyouxian
- * @date: Create in 2017-11-12 10:37
+ * @date: Create in 2017-12-26 21:01
  **/
-public class Main {
+public class Client {
     public static void main(String[] args) {
+
         SparkSession spark = SparkSession
                 .builder()
 //                .master("spark://10.77.40.27:7077")
-                .appName("1M-clean-data-index")
+                .appName("BBM-1M")
                 .getOrCreate();
-        JavaRDD<Point> point = spark.read().textFile(args[0]).javaRDD().map(new Function<String, Point>() {
+        JavaRDD<Point> point = spark.read().textFile("/rainbow-manage/Points.txt").javaRDD().map(new Function<String, Point>() {
             public Point call(String line) throws Exception {
-                String[] fields = line.split(args[1]);
+                String[] fields = line.split(",");
                 Point p = new Point(Integer.valueOf(fields[0]), Float.valueOf(fields[1]), Float.valueOf(fields[2]));
                 return p;
             }
         });
         Dataset<Row> dataset = spark.createDataFrame(point, Point.class);
-        dataset.createOrReplaceTempView(args[2]);
+        dataset.createOrReplaceTempView("Point");
         try {
-            Dataset<Row> pointDF = spark.sql(args[3]);
+            Dataset<Row> pointDF = spark.sql("select a.* from\n" +
+                    "(  select p1.ID, p1.PointX, p1.PointY, p2.ID as P2_ID, p2.PointX as P1_PointX, p2.PointY as P2_PointY, round((cast(p1.PointX as float) - cast(p2.PointX as float)), 4) * round((cast(p1.PointX as float) - cast(p2.PointX as float)), 4) as num from Point p1, Point p2) a where a.num = 4 and a.PointY = 2");
             pointDF.show();
         } catch (Exception e) {
             System.out.print("Error Info: " + e.getMessage());
